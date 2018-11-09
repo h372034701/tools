ea56070e92a569f5b64d8e1d5d319c04f97e972b
hellozh <h372034701@qq.com>
2018-11-09 21:29:11 +0800

    DebugUILine

---
+++ UGUI/DebugUILine.cs
@@ -0,0 +1,34 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class DebugUILine : MonoBehaviour
+{
+    static Vector3[] fourCorners = new Vector3[4];
+    void OnDrawGizmos()
+    {
+        return;
+        foreach (MaskableGraphic g in GameObject.FindObjectsOfType<MaskableGraphic>())
+        {
+            if (g.raycastTarget)
+            {
+                RectTransform rectTransform = g.transform as RectTransform;
+                rectTransform.GetWorldCorners(fourCorners);
+                var button = g.GetComponent<Button>();
+                if (button == null)
+                {
+                    Gizmos.color = Color.magenta;
+                    for (int i = 0; i < 4; i++)
+                        Gizmos.DrawLine(fourCorners[i], fourCorners[(i + 1) % 4]);
+                }
+                else
+                {
+                    Gizmos.color = Color.blue;
+                    for (int i = 0; i < 4; i++)
+                        Gizmos.DrawLine(fourCorners[i], fourCorners[(i + 1) % 4]);
+                }
+            }
+        }
+    }
+}
+#endif
